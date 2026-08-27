6fe820b62087578a329965ee7c51aa3fc72a8d2e
Luckylokeshwar <chilumulalokeshwar@gmail.com>
2026-08-27 17:24:04 +0530

    Create portfolio.sql

---
+++ database/portfolio.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE portfolio_db;
+
+USE portfolio_db;
+
+CREATE TABLE contacts (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+    email VARCHAR(100),
+    subject VARCHAR(200),
+    message TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
